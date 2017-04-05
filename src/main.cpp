7e3eb26ffef5127070f42ac771e1309b82ad57d3
autoiohuwenjie <huwenjiedeyouxiang@126.com>
2017-04-05 11:01:12 +0800

    delete useless files

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,13 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
 
-#include <trapezium.h>
-#include <radarline.h>
+#include "trapezium.h"
 
 int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
 
-    qmlRegisterType<RadarLine>("RadarLine", 1, 0, "RadarLine");
     qmlRegisterType<Trapezium>("Trapezium",1,0,"Trapezium");
-
 
     QQmlApplicationEngine engine;
     engine.load(QUrl(QStringLiteral("qrc:/qml/main.qml")));
